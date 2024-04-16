6262514861633e5e4d802ed4d38f68b694c511e8
karanpatel0608 <202152315@iiitvadodara.ac.in>
2024-04-17 02:05:12 +0530

    Naviating to Recipe Screen

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/widgets/meal_item.dart';
+import 'package:meals/screens/meal_details.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -8,35 +9,48 @@ class MealsScreen extends StatelessWidget {
     required this.title,
     required this.meals,
   });
-
   final String title;
   final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(meal: meal),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-
-      Widget content = Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Uh oh ...  Nothing Here',
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground)),
-            const SizedBox(height: 16),
-            Text('Try Selecting a different Category',
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                    color: Theme.of(context).colorScheme.onBackground))
-          ],
-        ),
-      );
-    
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Uh oh ...  Nothing Here',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground)),
+          const SizedBox(height: 16),
+          Text('Try Selecting a different Category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground))
+        ],
+      ),
+    );
 
     if (meals.isNotEmpty) {
       content = ListView.builder(
           itemCount: meals.length,
-          itemBuilder: (ctx, index) => MealItem(meal: meals[index])
-
-          );
-    } 
+          itemBuilder: (ctx, index) => MealItem(
+                meal: meals[index],
+                onSelectMeal: (meal) {
+                  selectMeal(context, meal);
+                },
+              ));
+    }
 
     return Scaffold(
         appBar: AppBar(
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -5,8 +5,9 @@ import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required  this.onSelectMeal });
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
@@ -28,7 +29,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(children: [
           FadeInImage(
             placeholder: MemoryImage(kTransparentImage),
@@ -66,13 +69,13 @@ class MealItem extends StatelessWidget {
                     const SizedBox(width: 12),
                     MealItemTrait(
                       icon: Icons.work,
-                      label:complexityText,
+                      label: complexityText,
                     ),
                     const SizedBox(width: 12),
                     MealItemTrait(
                       icon: Icons.attach_money,
-                      label:affordabilityText,
-                    ), 
+                      label: affordabilityText,
+                    ),
                   ],
                 )
               ]),
