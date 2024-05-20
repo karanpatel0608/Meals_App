f09956d8054ef522c42a4ceec9b1a9c01bc636bc
karanpatel0608 <202152315@iiitvadodara.ac.in>
2024-05-20 23:55:29 +0530

    adding some Implicit Animations like Image Expansion and Fav Icon Rotation

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -32,20 +32,35 @@ class MealDetailsScreen extends ConsumerWidget {
                   content: Text(
                       wasAdded ? "Meal Added as a favorite" : "Meal Removed"),
                 ),
-              ); 
+              );
             },
-            icon:  Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(    //RotationTransition is used to rotate the child widget           
+                  // turns: Tween<double>(begin: 0.5, end: 1).animate(animation),
+                  turns: Tween(begin: 0.85, end: 1.0).animate(animation),
+                  // turns: Tween(begin: 0.5, end: 1).animate(animation),  Ye Error dega toh upar wale do mese ek use kro
+
+                  child: child,
+                );
+              },
+              child: Icon(isFavorite ? Icons.star : Icons.star_border, key: ValueKey(isFavorite),),
+            ),
           ),
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                width: double.infinity,
+                height: 300,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(
               height: 14,
